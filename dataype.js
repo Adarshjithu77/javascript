b52833b41ee176ce48460523f5fe5f6507b1f97b
Adarshjithu77 <adarshjithu77@gmail.com>
2025-07-03 13:17:28 +0530

    datatype commit

---
+++ dataype.js
@@ -0,0 +1,33 @@
+//Data types
+
+
+// string
+
+let name ="Abhi"
+
+console.log(name)
+console.log(typeof name)
+
+
+//numberdatype
+let mobilenum=8589913698
+console.log(mobilenum)
+console.log(typeof mobilenum)
+
+
+//boolean datatype
+let booleannum=true
+console.log(booleannum)
+console.log(typeof booleannum)
+
+
+//undefined
+let age;
+console.log(age)
+console.log(typeof age)
+
+//null
+let sing=null
+
+console.log(sing)
+console.log(typeof sing)
--- function.js
+++ function.js
@@ -1,12 +1,89 @@
-function myFunc(){
-    console.log("function 1")
-}
+// function myFunc(){
+//     console.log("function 1")
+// }
 
-function myFunc2(){
-    console.log("function 2")
-    
-myFunc()
-}
+// function myFunc2(){
+//     console.log("function 2")
+//     a=20
+//     b=30
+//     c=a+b
+//     console.log(c)
+
+// myFunc()
+// }
 
 
-myFunc2()
+// myFunc2()
+
+
+// function addTwoNumber(){
+//     a=10;
+//     b=30;
+//     c=a+b
+//     return c
+// }
+
+
+
+
+// let sumFromThefunction=addTwoNumber()
+// console.log(sumFromThefunction)
+// let anotherNumber=50
+
+
+// let sum=anotherNumber+sumFromThefunction
+// console.log(sum)
+
+
+
+// function calculateSharonScore(){
+//     let javascript = 90;
+//     let html = 80;
+//     let css =100;
+
+
+//     let sum = javascript + html +css;
+//     return sum;
+// }
+
+
+// function calculateAbinScore(){
+//     let nodejs =80;
+//     let react =90;
+//     let angular = 60;
+
+//     let sum =nodejs + react + angular;
+
+//     return sum;
+// }
+
+// let sharonScore = calculateSharonScore()
+// console.log(sharonScore)
+
+// let abinScore =calculateAbinScore()
+// console.log(abinScore)
+
+
+// let totalScoreBoth = sharonScore+abinScore;
+// console.log(totalScoreBoth)
+
+
+
+// ! Type of functions
+
+
+let greet = function() {
+    console.log("=======================")
+    return "hello there";
+
+    console.log("heloo haiiiiii")
+};
+
+
+// let returnedValue =greet();
+// console.log(returnedValue);
+
+
+console.log(greet());
+let newValue =greet();
+console.log(newValue);
